084307c1c9a94583304f1c733a4f86e6e59e78e4
abcd9999971 <abcd9999971@gmail.com>
2025-08-31 01:57:57 +0900

    測試json

---
+++ frontend/page/src/components/posts.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li v-for="post in posts" :key="post.created_at ">
+            <div class="post">
+                <p class="time">{{post.created_at}}</p>
+                <p class="detail">{{post.text}}</p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+    import posts from '../post.json'
+</script>
+
