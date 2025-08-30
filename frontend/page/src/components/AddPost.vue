71e82f1e275fe9b0d5610aa8e243eb91e5f3c3a5
abcd9999971 <abcd9999971@gmail.com>
2025-08-31 01:58:25 +0900

    post/timeline相關組件

---
+++ frontend/page/src/components/AddPost.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="add-post-bar">
+        <input v-model="text" placeholder="What's new?" />
+        <button @click="handleAdd">Post</button>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const text = ref('')
+const emit = defineEmits(['add'])
+
+
+function handleAdd() {
+    if (text.value.trim() !== '') {
+        emit('add', text.value)
+        text.value = ''
+    }
+    else {
+        alert('Post cannot be empty!')
+}
+}
+
+</script>
+
+<style scoped>
+
+</style >
---
+++ frontend/page/src/components/TimelineBar.vue
@@ -0,0 +1,53 @@
+<template>
+<ul id='timeline'>
+    <li v-for="i in index" :key="i" class="post">
+    <div class="dot"></div>
+    <div class="content">test{{ i }}</div>
+</li>
+</ul>
+</template>
+
+<script setup>
+const index = Array.from({ length: 20 }, (_, i) => i + 1)
+</script>
+
+
+<style scoped>
+
+#timeline{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    position: relative;
+}
+
+#timeline::before{
+    content: '';
+    position: absolute;
+    width: 2px;
+    background-color: #ccc;
+    top: 8px;
+    left: 50%;
+    height: 100%;
+    transform: translateX(-50%);
+    z-index: 0;
+}
+
+#timeline .post{
+    position: relative;
+    min-height: 60px;
+}
+#timeline .dot{
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 12px;
+    height: 12px;
+    background-color: #000000;
+    border-radius: 50%;
+    transform: translateX(-50%);
+    z-index: 2; /* make sure dot is above the timeline */
+}
+
+
+</style>
