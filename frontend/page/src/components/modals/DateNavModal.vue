7de9031017e5a061045f183e6b44959b0d32a722
abcd9999971 <abcd9999971@gmail.com>
2025-09-05 14:08:54 +0900

    add new file for segmentation

---
+++ frontend/page/src/components/modals/DateNavModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Component content goes here -->
+  </div>
+</template>
+
+<script setup>
+// Component logic goes here
+</script>
+
+<style scoped>
+/* Component styles go here */
+</style>
