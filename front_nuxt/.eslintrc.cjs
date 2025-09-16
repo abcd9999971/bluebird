43a991af74094920fc6ae60a5598f2b5b735a9e1
abcd9999971 <abcd9999971@gmail.com>
2025-09-16 17:02:07 +0900

    feat: eslint/tailwind setting

---
+++ front_nuxt/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2023: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier' 
+  ],
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'module'
+  },
+  plugins: ['prettier'],
+  rules: {
+    'no-under': 'off',
+    'prettier/prettier': 'error'
+  }
+}
