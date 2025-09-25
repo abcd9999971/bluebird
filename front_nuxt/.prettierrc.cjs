37562ad8345a9ef1bfe0ae48b1bf4c1b7268ee76
abcd9999971 <abcd9999971@gmail.com>
2025-09-25 16:07:54 +0900

    format: add Tab setting to .prettierrc

--- front_nuxt/.prettierrc.cjs
+++ front_nuxt/.prettierrc.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-    plugins: [
-        'prettier-plugin-tailwindcss'
-    ],
-    printWidth: 100,
-    semi: false,
-    singleQuote: true, 
-    trailingComma: 'none',
+	plugins: ['prettier-plugin-tailwindcss'],
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	useTabs: true
 }
